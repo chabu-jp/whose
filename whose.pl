b5876758ba58993201af8216cbf4bcb6613fc456
chabu <chabu@users.noreply.github.com>
2018-05-02 07:14:15 +0900

    unreadable

--- whose.pl
+++ whose.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Should be compatible with Perl 5.8.x
+# should be compatible with Perl 5.8.x
 
 use strict;
 use warnings;
@@ -24,10 +24,9 @@ print "\n", 'IP Address: ', $ip_address, "\n";
 
 PTR: {
 	my $packet = $resolver->query($ip_address);
-	
+
 	if ($packet) {
-		foreach my $rr ($packet->answer)
-		{
+		foreach my $rr ($packet->answer) {
 			if ($rr->type eq 'PTR') {
 				printf "Hostname  : %s\n\n", $rr->ptrdname;
 			}
@@ -39,7 +38,7 @@ PTR: {
 
 AUTHORITY: {
 	my $packet = $resolver->send($ip_address, 'SOA', 'IN');
-	
+
 	if ($packet->authority) {
 		my ($rr) = $packet->authority;
 		if ($rr->type eq 'SOA') {
@@ -60,7 +59,7 @@ my @asn;
 
 PREFIX_INFO: {
 	my $qname;
-	
+
 	if (index($ip_address, ':') == -1) {
 		my @v4 = split(/\./, $ip_address);
 		$qname = join('.', @v4[2, 1, 0]) . '.origin.asn.cymru.com.';
@@ -72,15 +71,14 @@ PREFIX_INFO: {
 		my @v6rev = reverse @v6chr;
 		$qname = join('.', @v6rev) . '.origin6.asn.cymru.com.';
 	}
-	
+
 	my $packet = $resolver->query($qname, 'TXT', 'IN');
-	
+
 	if ($packet) {
-		foreach my $rr ($packet->answer)
-		{
+		foreach my $rr ($packet->answer) {
 			if ($rr->type eq 'TXT') {
 				my @cols = split(/\s?\|\s?/, $rr->txtdata, -1);
-				
+
 				printf "Prefix    : %s\n", $cols[1];
 				printf "Origin    : %s\n", $cols[0];
 				printf "Country   : %s (%s)\n", $cols[2], uc($cols[3]);
@@ -96,16 +94,14 @@ my %asn = map { ($_, undef) } @asn;
 @asn = sort { $a <=> $b } keys %asn;
 
 AS_INFO: {
-	foreach my $asn (@asn)
-	{
+	foreach my $asn (@asn) {
 		my $packet = $resolver->query("AS$asn.asn.cymru.com.", 'TXT', 'IN');
-		
+
 		if ($packet) {
-			foreach my $rr ($packet->answer)
-			{
+			foreach my $rr ($packet->answer) {
 				if ($rr->type eq 'TXT') {
 					my @cols = split(/\s?\|\s?/, $rr->txtdata, -1);
-					
+
 					printf "ASN       : %s\n", $cols[0];
 					printf "Descr     : %s\n", $cols[4];
 					printf "Country   : %s (%s)\n", $cols[1], uc($cols[2]);
